fb92d7dab41b6953aabfae88333a2da576e23ea6
Alex <lange.alexander222@gmail.com>
2014-06-12 22:13:04 -0400

    Removed duplicates

--- uva/10004/Main.java
+++ uva/10004/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.ArrayList;
 
-public class Main{
+public class Main{ 
     public static void main(String[] args){
 	Scanner scan = new Scanner(System.in);
 
